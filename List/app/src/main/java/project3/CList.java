d3145ca8d723bdb93dc91ebbf6dd4477ece7fde8
lyhdra99 <lyhdra99@gmail.com>
2023-01-23 23:42:42 +0900

    init

---
+++ List/app/src/main/java/project3/CList.java
@@ -0,0 +1,46 @@
+package project3;
+
+public class CList<T> {
+
+    private Node<T> last;
+    private int size;
+
+    public CList(){
+        last = null;
+        size = 0;
+    }
+
+    public void insert(T item){
+        if (last == null) {
+            last = new Node<T>(item);
+            last.setNext(last);            
+        }else{
+            Node<T> newItem = new Node<T>(item);
+            newItem.setNext(last.getNext());
+            this.last.setNext(newItem);            
+        }
+        size++;
+    }
+
+    public void delete(T item){
+        Node<T> obj = last;
+        while (obj.getNext() != last) {
+            Node<T> previous = obj;
+            obj = obj.getNext();
+            if (obj.getItem() == item) {
+                previous.setNext(obj.getNext());
+                break;
+            }            
+        }
+    }
+
+    public void displayAll(){
+        Node<T> obj = last;
+        String result = "";
+        while ((obj = obj.getNext()) != last) {
+            result += obj.getItem().toString();
+        }
+        System.out.println(result);
+    }
+
+}
---
+++ List/app/src/main/java/project3/Node.java
@@ -0,0 +1,29 @@
+package project3;
+
+public class Node<T> {
+
+    private T item;
+    private Node<T> next;
+
+    public Node(T item){
+        this.item = item;
+        this.next = null;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+}
